1806527fc8598b1672208641346b10af0877c5c2
Karyn Huston <huston.karyn@gmail.com>
2023-05-07 12:00:50 -0400

    tested get_an_image function for -> text to image

--- controllers/reading-assistance.js
+++ controllers/reading-assistance.js
@@ -2,16 +2,21 @@ const express = require("express");
 const ra = express.Router();
 
 const { askAquestion } = require("../queries/api-usage.js");
+const { get_an_image } = require("./text-to-image-prompt.js");
+ra.post("/image", async (req, res) => {
+  const { question } = req.body;
 
-ra.post("/", async (req, res) => {
-  const {question} = req.body;
-  console.log("testing in ra");
-  try {
-    const recieveQuestion = await askAquestion(req.body);
-    res.json(recieveQuestion);
-  } catch (error) {
-    res.status(400).json({ error: error });
-  }
+  const result = await get_an_image("cute bunny");
+  console.log(result)
+  console.log(Object.keys(result), result.response.data.error);
+  res.send(JSON.stringify(result));
+  // console.log("testing in ra");
+  // try {
+  //   const recieveQuestion = await askAquestion(req.body);
+  //   res.json(recieveQuestion);
+  // } catch (error) {
+  //   res.status(400).json({ error: error });
+  // }
 });
 
 module.exports = ra;
--- controllers/text-to-image-prompt.js
+++ controllers/text-to-image-prompt.js
@@ -1,10 +1,20 @@
 const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const { openai_api_key } = require("../token.js");
+const configuration = new Configuration({ apiKey: openai_api_key }); 
 const openai = new OpenAIApi(configuration);
-const response = await openai.createImage({
-  prompt: "A cute baby sea otter",
-  n: 2,
-  size: "1024x1024",
-});
+
+async function get_an_image(prompt) {
+  try {
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    console.log(error.response.data.error)
+  }
+}
+
+module.exports = { get_an_image };

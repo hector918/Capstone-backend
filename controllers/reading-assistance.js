74c8c641543fe23307747b77de0725cdf62ab3f6
hector918 <hector_zhong@outlook.com>
2023-05-07 12:47:23 -0400

    reading assistance text to image add desciption

--- controllers/reading-assistance.js
+++ controllers/reading-assistance.js
@@ -8,10 +8,14 @@ const {insert_to_api_usage} = require('../queries/api-usage');
 ra.post("/image", async (req, res) => {
   try {
     let {q} = req.body;
+    //pre filter the user input
     question = user_input_filter(q);
     if(question === "" || question.length < 4 || question.length > 1000) throw "question invaild";
+    //call open ai api
     const ret = await get_an_image(q);
+    //record api usage
     insert_to_api_usage({user_name: req.sessionID, user_input: q, caller: 'reading-assistance-text-to-image', json: ret});
+    //respond
     res.json({result: "success", image_url: ret.data[0].url});
   } catch (error) {
     console.log(error);

d7e69d6ef6d854971b60e47c7d82cc70a5c54917
hector918 <hector_zhong@outlook.com>
2023-07-09 15:00:27 -0400

    login function

--- controllers/user-control.js
+++ controllers/user-control.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const uc = express.Router();
-const {check_userID_available, user_password_hash, create_an_user} = require('../queries/user-control');
-const {userIdRegex, passwordRegex} = require('./str-filter');
+const {check_userID_available, user_password_hash, create_an_user, login} = require('../queries/user-control');
+const {userIdRegex, passwordRegex, user_input_filter} = require('./str-filter');
 //////////////////////////////////////
 uc.get("/available", async(req, res) => {
   console.log(req.session,req.sessionID,"a")
@@ -19,23 +19,55 @@ uc.post('/register', async(req, res) => {
     let {userId, password} = req.body;
     //define new user json
     let newUserJson = { 
-      userId, 
+      userid: userId, 
       password: user_password_hash(password), 
       availability: true, 
       current_session: req.sessionID,
-      userName: generateUsername(),
-      last_seen: new Date(),
+      username: generateUsername(),
+      last_seen: new Date().toUTCString(),
       ip_address: req.socket.remoteAddress,
     }
     console.log(newUserJson);
     //insert in to db
     const ret = await create_an_user(newUserJson);
     if(ret !== false){
-      res.json({userId: ret.userId});
+      res.json({userId: ret.userid});
     } else throw req.trans("Add user failed.");
   } catch (error) {
     console.error(error);
     res.status(500).json({error});
+  }
+});
+
+uc.get('/logout', async (req,res) => {
+  try {
+    console.log(req.session.user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({"error":"error"});
+  }
+});
+
+uc.post("/login", async(req, res) => {
+  try {
+    let {userId, password} = req.body;
+    const ret = await login({
+      userid: user_input_filter(userId), password: user_input_filter(password)
+    })
+    if(ret===false){
+      //if login failed
+      res.json({error: req.trans("User ID or password not matched.")})
+    }else{
+      //if login successed
+      const templete = {"username":"text",  "last_seen":"text", "credit": "text", "profile_setting": "json"};
+      for(let x in templete) if(ret[x]) templete[x] = ret[x];
+      res.json(templete);
+      req.session.userInfo = templete;
+      req.session.save();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: req.trans("Ooops..., contact Admin please.")});
   }
 });
 
--- queries/user-control.js
+++ queries/user-control.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 
 const create_an_user = async (newUserJson) => {
   const [col_name, val_name] = [[], []];
-  const templete = {"userId":"text", "userName":"text", "current_session":"text", "password":"text", "last_seen":"text", "availability":"bool", "ip_address":"text"};
+  const templete = {"userid":"text", "username":"text", "current_session":"text", "password":"text", "last_seen":"text", "availability":"bool", "ip_address":"text"};
   for(let key in templete) if(newUserJson[key]){
     col_name.push(key);
     val_name.push(`$[${key}]`);
@@ -20,6 +20,18 @@ const create_an_user = async (newUserJson) => {
 
 };
 
+const login = async (userForm) => {
+  try {
+    // const ret = await db.one(`SELECT * FROM public.user WHERE availability = 'true' AND userid = $[userid] and password = $[password]`, userForm);
+    const ret = await db.one(`UPDATE public.user SET last_seen = '${new Date().toUTCString()}' WHERE availability = 'true' AND userid = $[userid] and password = $[password] RETURNING *;`, userForm);
+    console.log(ret);
+    return ret;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
 const check_userID_available = async (userID) => {
   try {
     const ret = await db.oneOrNone(`SELECT current_session FROM "user" WHERE "userId" = $[userID]`, {userID});
@@ -33,4 +45,4 @@ const user_password_hash = (password) => {
   return crypto.createHash('sha256').update(password).digest('hex');
 }
 
-module.exports = { create_an_user, check_userID_available, user_password_hash };
+module.exports = { create_an_user, check_userID_available, user_password_hash, login };

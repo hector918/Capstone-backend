dd20862041087a1c53064cfddf865df208e1ced3
hector918 <hector_zhong@outlook.com>
2023-07-18 10:12:45 -0400

    add user log, user documents table

--- controllers/user-control.js
+++ controllers/user-control.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const uc = express.Router();
-const {check_userID_available, user_password_hash, create_an_user, login} = require('../queries/user-control');
+const {check_userID_available, user_password_hash, create_an_user, login, log_user_action} = require('../queries/user-control');
 const {userIdRegex, passwordRegex, user_input_filter} = require('./str-filter');
 //////////////////////////////////////
 uc.get("/available", async(req, res) => {
@@ -19,7 +19,7 @@ uc.post('/register', async(req, res) => {
     let {userId, password} = req.body;
     //define new user json
     let newUserJson = { 
-      userid: userId, 
+      user_id: userId, 
       password: user_password_hash(password), 
       availability: true, 
       current_session: req.sessionID,
@@ -31,8 +31,8 @@ uc.post('/register', async(req, res) => {
     //insert in to db
     const ret = await create_an_user(newUserJson);
     if(ret !== false){
-      res.json({userId: ret.userid});
-    } else throw req.trans("Add user failed.");
+      res.json({userId: ret.user_id});
+    }else throw req.trans("Add user failed.");
   } catch (error) {
     console.error(error);
     res.status(500).json({error: error.message});
@@ -41,9 +41,10 @@ uc.post('/register', async(req, res) => {
 
 uc.get('/logout', async (req,res) => {
   try {
+    log_user_action(req.session.userInfo.userId, "user logout");
     req.session.userInfo = {};
     req.session.save();
-    res.json({data: req.trans("Successed logout.")})
+    res.json({data: req.trans("Successed logout.")});
   } catch (error) {
     console.error(error);
     res.status(500).json({"error": "error"});
@@ -54,7 +55,7 @@ uc.post("/login", async(req, res) => {
   try {
     let {userId, password} = req.body;
     const ret = await login({
-      userid: user_input_filter(userId), password: user_input_filter(password)
+      user_id: user_input_filter(userId), password: user_input_filter(password)
     })
     if(ret === false){
       //if login failed
@@ -64,6 +65,8 @@ uc.post("/login", async(req, res) => {
       const templete = {"username":"text",  "last_seen":"text", "credit": "text", "profile_setting": "json"};
       for(let x in templete) if(ret[x]) templete[x] = ret[x];
       res.json({data: templete});
+      //add user id back to session
+      templete['userId'] = ret['user_id'];
       req.session.userInfo = templete;
       req.session.save();
     }
@@ -96,16 +99,21 @@ uc.post('/check_userID', async(req, res) => {
 });
 //////////////////////////////////////
 function VerifyUserLogin(req, res, next){
-  console.log("in verify user login",req.session.userInfo);
-  if(req.session.userInfo === undefined){
-    //new session, no login also
-  }else if(req.session.userInfo.userId === undefined){
-    //was login, but logout already
-  }else if(req.session.userInfo.userId){
-    //login user
+  try {
+    if(req.session.userInfo === undefined){
+      //new session, no login also
+      throw new Error (req.trans("You need to login first."));
+    }else if(req.session.userInfo.userId === undefined){
+      //was login, but logout already
+      throw new Error (req.trans("You need to login first."));
+    }else if(req.session.userInfo.userId){
+      //login user
+      //if success
+      next();
+    }
+  } catch (error) {
+    res.json({"error": error.message});
   }
-  //if success
-  next();
 }
 function generateUsername() {
   var adjectives = ['happy', 'sad', 'funny', 'serious', 'clever', 'smart', 'kind', 'brave', 'shiny', 'silly', 'energetic', 'graceful', 'playful', 'witty', 'gentle', 'curious', 'charming', 'vibrant', 'daring', 'fantastic'];

57be8de757482166f50a6a1cd159c145d766a75e
hector918 <hector_zhong@outlook.com>
2023-08-06 04:57:50 -0400

    landing css update

--- server.js
+++ server.js
@@ -14,17 +14,23 @@ const options = {
 
 // LISTEN
 try {
-  https.createServer(options, app).listen(PORT, () => {
+  const https_server = https.createServer(options, app).listen(PORT, () => {
     console.log(`Https Listening on port ${PORT}`);
   });
-  http.createServer(app).listen(PORT -1, () => {
+  const http_server = http.createServer(app).listen(PORT -1, () => {
     console.log(`Http Listening on port ${PORT -1}`);
   });
+
+  https_server.on('clientError', handle_client_error);
+  http_server.on('clientError', handle_client_error);
 } catch (error) {
   console.log(error);
 }
 
-
+function handle_client_error(error, socket) {
+  console.error('clientError:', error);
+  socket.destroy();  // This will destroy the socket
+}
 // app.listen(PORT, () => {
 //   console.log(`Listening on port ${PORT}`);
 // });

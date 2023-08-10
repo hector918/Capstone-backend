edd5de372508a10652645e307fa3d3e4ac158a39
hector918 <hector_zhong@outlook.com>
2023-08-10 07:29:07 -0400

    working with chatting

--- controllers/chatting-with-openai.js
+++ controllers/chatting-with-openai.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cwo = express.Router();
 const {chatCompletion} = require('./wrapped-api');
 const { listen_to_sse, hosting_sse } = require('./server-sent-event');
+const {verifyUserLogin} = require('./user-control');
 //////////////////////////////////////////
-cwo.post('/openai/SSE', (req, res) => {
+cwo.post('/openai/SSE', verifyUserLogin, (req, res) => {
   try {
     const {messages} = req.body;
-    const response = chatCompletion('gpt-3.5-turbo-16k', messages, 2);
+    const response = chatCompletion('gpt-3.5-turbo-16k', messages, 1);
     //handle openai sse 
     if(response){
       const stream_handler = hosting_sse(req, res);

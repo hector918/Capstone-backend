080ef4419011fd343663789f11ff6a015be1da53
hector918 <hector_zhong@outlook.com>
2023-08-12 05:21:48 -0400

    adding prompt panel function

--- controllers/prompt-library.js
+++ controllers/prompt-library.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const prompt = express.Router();
-const {readAllPromptByUser, insertNewPrompt} = require("../queries/prompt-library");
+const {readAllPromptByUser, insertNewPrompt, deletePrompt} = require("../queries/prompt-library");
 const {verifyUserLogin} = require('./user-control');
 ///////////////////////////////////
 prompt.post('/new', verifyUserLogin, async(req, res) => {
   try {
-    const {type, prompt, title} = req.body;
+    const {type, prompt, title, linkslist} = req.body;
     const {userId} = req.session.userInfo;
-    const ret = await insertNewPrompt(userId, type, title, prompt);
-    console.log(ret);
+    const ret = await insertNewPrompt(userId, type, title, prompt, {links: linkslist});
     if(ret){
       res.json({data: ret});
     }else{
@@ -25,6 +24,20 @@ prompt.get("/", verifyUserLogin, async(req, res) => {
     const {userId} = req.session.userInfo;
     const ret = await readAllPromptByUser(userId);
     res.json({data: ret});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: error.message});
+  }
+})
+
+prompt.delete('/:prompt_id', verifyUserLogin, async(req, res) => {
+  try {
+    const {userId} = req.session.userInfo;
+    const {prompt_id} = req.params;
+    const ret = await deletePrompt(userId, prompt_id);
+    console.log(ret);
+    if(ret === null) throw new Error("nothing is deleted");
+    res.json({data: "success"});
   } catch (error) {
     console.error(error);
     res.status(500).json({error: error.message});
--- queries/prompt-library.js
+++ queries/prompt-library.js
@@ -2,20 +2,20 @@ const db = require("./db-config");
 const {timestamp, error_handle} = require('./db-general');
 
 
-const insertNewPrompt = async(user_id, type, title, content) => {
+const insertNewPrompt = async(user_id, type, title, content, json) => {
   try {
     const ret = await db.one(`
     WITH prompts AS (
-      INSERT INTO prompts (type, content, title, timestamp)
-      VALUES ($[type], $[content], $[title], $[timestamp])
+      INSERT INTO prompts (type, content, title, json, timestamp)
+      VALUES ($[type], $[content], $[title], $[json], $[timestamp])
       RETURNING *
     ),
     user_to_prompt AS (
       INSERT INTO user_to_prompt (user_id, prompt_id, timestamp)
       SELECT $[user_id], id, $[timestamp] FROM prompts RETURNING *
     )
-    SELECT * FROM prompts, user_to_prompt;
-    `, {user_id, title, type, content, ...timestamp()});
+    SELECT prompts.*, user_to_prompt.timestamp FROM prompts, user_to_prompt;
+    `, {user_id, title, type, content, json, ...timestamp()});
     return ret;
   } catch (error) {
     error_handle(error);
@@ -37,4 +37,24 @@ const readAllPromptByUser = async(user_id) => {
   }
 }
 
-module.exports = {insertNewPrompt, readAllPromptByUser}
+const deletePrompt = async(user_id, prompt_id) => {
+  try {
+    const ret = await db.oneOrNone(`
+      with deleted_user_to_prompt AS (
+        DELETE FROM user_to_prompt WHERE user_id = $[user_id] and prompt_id = $[prompt_id] RETURNING *
+      )
+      DELETE FROM prompts where id = (SELECT prompt_id FROM deleted_user_to_prompt) RETURNING *;
+    `, {user_id, prompt_id});
+    return ret;
+  } catch (error) {
+    error_handle(error);
+    return false;
+  }
+}
+
+
+module.exports = {
+  insertNewPrompt, 
+  readAllPromptByUser,
+  deletePrompt
+}
